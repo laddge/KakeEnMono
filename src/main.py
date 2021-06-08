157cc2611ce23a9d888f1795913eef04b201565e
Laddge <dev.laddge@gmail.com>
2021-06-08 04:02:19 +0000

    [feat] ディレクトリ構造を変更

---
+++ src/main.py
@@ -0,0 +1,16 @@
+#!fontforge -lang=py -script
+
+import os
+import fontforge
+
+
+def main():
+    FONTDIR = os.path.join(os.path.dirname(os.path.abspath(__file__)), '../fonts')
+    font = fontforge.font()
+    font.mergeFonts(os.path.join(FONTDIR, 'InputMono-Regular.ttf'))
+    font.mergeFonts(os.path.join(FONTDIR, 'togoshi-mono.ttf'))
+    font.generate(os.path.join(FONTDIR, 'Sova_Mono-Regular.ttf'))
+
+
+if __name__ == '__main__':
+    main()
