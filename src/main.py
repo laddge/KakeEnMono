04980e790891f28a5fd855ccba660a6221251c97
Laddge <dev.laddge@gmail.com>
2021-06-12 07:40:32 +0000

    [feat] フォント情報を記述

--- src/main.py
+++ src/main.py
@@ -20,6 +20,19 @@ def main():
     font.selection.select(0x3000)
     font.pasteInto()
     font.intersect()
+    font.copyright = '''Copyright (c) 2021 Laddge
+[Ubuntu Mono]
+Copyright 2011 Canonical Ltd.  Licensed under the Ubuntu Font Licence 1.0
+
+[Source Han Sans]
+Copyright (c) 2014, 2015 Adobe Systems Incorporated (http://www.adobe.com/), with Reserved Font Name 'Source'.
+
+[M+ OUTLINE FONTS]
+Copyright(c) 2015 M+ FONTS PROJECT'''
+    font.familyname = 'KakeEn Mono'
+    font.fontname = 'KakeEnMono-Regular'
+    font.fullname = 'KakeEn Mono'
+    font.version = '1.0.0'
     font.generate(os.path.join(PROJECTDIR, 'fonts/KakeEnMono-Regular.ttf'))
 
 
