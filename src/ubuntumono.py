15348735f724c75dd2bfba80c196e492fba32246
Laddge <dev.laddge@gmail.com>
2021-06-12 09:12:39 +0000

    [feat] 大カッコのサイズを調整

--- src/ubuntumono.py
+++ src/ubuntumono.py
@@ -1,11 +1,17 @@
 import os
 import fontforge
+import psMat
 
 
 def main():
     PROJECTDIR = os.path.join(os.path.dirname(os.path.abspath(__file__)), '../')
     font_path = os.path.join(PROJECTDIR, 'fonts/UbuntuMono-Regular.ttf')
     font = fontforge.open(font_path)
+    for glyph in font.glyphs():
+        if glyph.unicode in [0x5b, 0x5d]:
+            width = glyph.width
+            glyph.transform(psMat.scale(1.075))
+            glyph.width = width
     save_path = os.path.join(PROJECTDIR, 'fonts/UbuntuMono-Regular-edited.ttf')
     font.generate(save_path)
     return save_path
