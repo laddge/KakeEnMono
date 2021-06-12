db918f34ed1fc24ac50296e760a0b93521c6cb4e
Laddge <dev.laddge@gmail.com>
2021-06-12 05:29:00 +0000

    [feat] 全角スペースを可視化

--- src/main.py
+++ src/main.py
@@ -10,6 +10,16 @@ def main():
     genjyuu_fn = genjyuu.main()
     font = fontforge.open(ubuntumono_fn)
     font.mergeFonts(genjyuu_fn)
+    font.selection.none()
+    font.selection.select(0x2610)
+    font.copy()
+    font.selection.select(0x3000)
+    font.paste()
+    font.selection.select(0x271a)
+    font.copy()
+    font.selection.select(0x3000)
+    font.pasteInto()
+    font.intersect()
     font.generate(os.path.join(PROJECTDIR, 'fonts/Sova_Mono-Regular.ttf'))
 
 
